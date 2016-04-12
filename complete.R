069bbb2d480ea52dc21cb988d62309624f8f7f7a
markpawloski <mark@pawloski.net>
2016-04-11 23:27:57 -0400

    Delete complete.R

--- complete.R
+++
@@ -1,8 +0,0 @@
-complete <- function(directory, id=1:332){
-  f <- function(i){
-    data = read.csv(paste(directory, "/", sprintf("%03d", as.numeric(i)), ".csv", sep = ""))
-    sum(complete.cases(data))
-  }
-  nobs = sapply(id,f)
-  return(data.frame(id, nobs))
-}
