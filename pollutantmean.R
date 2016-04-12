1ba2ae63734e435d15c6c2a8386d3055e969b8ac
Mark Pawloski <mark@pawloski.net>
2016-04-11 23:26:19 -0400

    deleted:    complete.R 	deleted:    corr.R 	deleted:    pollutantmean.R

--- pollutantmean.R
+++
@@ -1,7 +0,0 @@
-pollutantmean <- function(directory, pollutant, id = 1:332){
-  fileList = list.files(directory)
-  selected = fileList[match(id,as.numeric(sub(".csv$","",fileList)))]
-  Data = lapply(file.path(directory,selected),read.csv)
-  Data = do.call(rbind.data.frame, Data)
-  mean(Data[,pollutant],na.rm=TRUE)
-}
